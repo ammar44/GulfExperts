c06a64e15b5c8ba83890f4dcb5245ccfd58ea849
Abdullah Alammar <shababksa25@hotmail.com>
2017-12-07 22:21:23 -0500

    add realtime database

--- app/src/main/java/com/example/shaba/gulfexperts/Activities/MainActivity.java
+++ app/src/main/java/com/example/shaba/gulfexperts/Activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.placeholder.PlaceHolderView;
 import com.example.shaba.gulfexperts.Model;
@@ -17,6 +18,11 @@ import com.example.shaba.gulfexperts.placeholderClasses.Utils;
 import com.example.shaba.gulfexperts.placeholderClasses.drawer.DrawerHeader;
 import com.example.shaba.gulfexperts.placeholderClasses.drawer.DrawerMenuItem;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mindorks.butterknifelite.ButterKnifeLite;
 import com.mindorks.butterknifelite.annotations.BindView;
 
@@ -58,6 +64,41 @@ Button advaneceSearchBtn , simpleSearchBtn, aboutUsBtn , sponsorsBtn ,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ////////////////////////////////////////////////////////////////////
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("gulfexperts-2017");
+
+        myRef.setValue("Hello, World!");
+
+
+
+
+
+// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                //Log.d(TAG, "Value is: " + value);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+                Toast.makeText(MainActivity.this, error.toException().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//////////////////////////////////////////////////////////////////////////
+        
+        
         Intent intent = getIntent();
         ButterKnifeLite.bind(this);
         setupDrawer();
